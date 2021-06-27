8da021d5e650b9bde2f0b56bf510b822d55c95bb
Alexander Pieper <alexanderpieper@alexanders-imac.fritz.box>
2021-06-27 16:46:09 +0200

    error handling and video added

--- analysis.py
+++ analysis.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 def main():
     # read the data
     df = pd.read_csv("export.csv", index_col = 0)
+    df = df.drop("2021-06-03 21:51:13", axis = 0)
+    
     df.index = pd.DatetimeIndex(df.index)
     print("Correlation Analysis:")
     print(df.corr())
@@ -13,19 +15,34 @@ def main():
     # if you dont want minutely data:
     # df = df[::5]
 
-    # plot
+    # # plot
+    # fig,ax = plt.subplots(1,1,
+    #                     figsize = (15,6))
+
+    # ax.plot(df["Count_original"], label = "Original", color = "tab:orange")
+    # ax1 = ax.twinx()
+    # ax1.plot(df["Count_LoFi"], label = "LoFi", color = "tab:blue")
+    # ax1.legend(loc = "upper right")
+    # ax.legend(loc = "upper left")
+    # ax.spines["top"].set_visible(False)
+    # ax1.spines["top"].set_visible(False)
+    # fig.tight_layout()
+    # plt.show()
+    
+    
     fig,ax = plt.subplots(1,1,
                         figsize = (15,6))
 
     ax.plot(df["Count_original"], label = "Original", color = "tab:orange")
-    ax1 = ax.twinx()
-    ax1.plot(df["Count_LoFi"], label = "LoFi", color = "tab:blue")
-    ax1.legend(loc = "upper right")
+    ax.plot(df["Count_LoFi"], label = "LoFi", color = "tab:blue")
+    ax.plot(df["Count_Nature"], label = "Nature", color = "tab:green")
+    ax.plot(df.sum(axis = 1), label = "All Viewers", color = "black")
     ax.legend(loc = "upper left")
     ax.spines["top"].set_visible(False)
-    ax1.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
     fig.tight_layout()
     plt.show()
+
 
 if __name__ == "__main__":
     main()
--- youtube_crawler.py
+++ youtube_crawler.py
@@ -24,26 +24,31 @@ class Video:
             export_df = pd.DataFrame(columns = self.col_names)
         
         while True:
+            start = time.time()
             time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             
             try:
                 for col_name, url in self.dict.items():
-                    # get desired Count from API
-                    http = requests.Session()
-                    response = http.get(url + '&key=' + self.api_key).text
-                    response_json = json.loads(response)
-                    export_df.loc[time_now, col_name] = int(response_json["items"][0]["liveStreamingDetails"]["concurrentViewers"])
-                
+                    try:
+                        # get desired Count from API
+                        http = requests.Session()
+                        response = http.get(url + '&key=' + self.api_key).text
+                        response_json = json.loads(response)
+                        export_df.loc[time_now, col_name] = int(response_json["items"][0]["liveStreamingDetails"]["concurrentViewers"])
+                    except:
+                        id = url.split("&id=")[1]
+                        print(f"Error for Stream with key: {id}. It probably stopped.")
                 print(export_df)
                 # export every hour
                 if pd.DatetimeIndex(export_df.index).hour[-1] != pd.DatetimeIndex(export_df.index).hour[-2]:
-                    export_df[col_names] = export_df[col_names].astype("Int64")
+                    export_df[self.col_names] = export_df[self.col_names].astype("Int64")
                     export_df.to_csv(self.file_name)
                 
-                time.sleep(59.8)
+                end = time.time()
+                time.sleep(60 - (end - start) - 1/24)
             except:
                 # export when, an error occurs
-                export_df[col_names] = export_df[col_names].astype("Int64")
+                export_df[self.col_names] = export_df[self.col_names].astype("Int64")
                 export_df.to_csv(self.file_name)
                 break
             
@@ -51,9 +56,12 @@ class Video:
 
 if __name__ == "__main__":
     urls = ['https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=yxdOhAyVrak',
-            'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=9UGEwjFiR8Q']
+            'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=auhMEZD3F1E',
+            'https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=ukwNqcZXLDk'
+            ]
     col_names = ["Count_original", 
-                 "Count_LoFi"]
+                 "Count_LoFi",
+                 "Count_Nature"]
 
     Videos = Video(urls = urls,
                     col_names = col_names,
